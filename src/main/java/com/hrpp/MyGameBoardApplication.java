eaddc0b8fa26b3b01cd77f67d47bb5646c7d56d7
KHR <rimiha9708@gmail.com>
2021-06-08 16:03:00 +0900

    환경세팅
    환경세팅

---
+++ src/main/java/com/hrpp/MyGameBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hrpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrpp/MyGameBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGameBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
